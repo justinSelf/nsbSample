8269378bbcc5878decd05b33208e40664d385265
Justin Self <justn.self@gmail.com>
2015-09-11 23:03:15 -0500

    stage 2

--- ProcessHandler/PlaceOrderHandler.cs
+++ ProcessHandler/PlaceOrderHandler.cs
@@ -11,10 +11,18 @@ namespace ProcessHandler
 {
     public class PlaceOrderHandler : IHandleMessages<PlaceOrder>
     {
+        private IBus _bus;
+
+        public PlaceOrderHandler(IBus bus)
+        {
+            _bus = bus;
+        }
+
         public void Handle(PlaceOrder message)
         {
             Console.WriteLine("Placing Order...");
             Console.WriteLine("Order {0} Placed!", message.Id);
+            _bus.Publish(new OrderPlaced { Id = message.Id });
         }
     }
 }
