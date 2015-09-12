8749a43bf53dae78445aa5fea22026fb8c0f182d
Justin Self <justn.self@gmail.com>
2015-09-11 22:55:20 -0500

    commit stage 1

---
+++ ProcessHandler/PlaceOrderHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+using Sample.Contracts;
+
+namespace ProcessHandler
+{
+    public class PlaceOrderHandler : IHandleMessages<PlaceOrder>
+    {
+        public void Handle(PlaceOrder message)
+        {
+            Console.WriteLine("Placing Order...");
+            Console.WriteLine("Order {0} Placed!", message.Id);
+        }
+    }
+}
